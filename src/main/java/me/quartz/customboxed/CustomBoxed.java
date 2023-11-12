173985895f5f380af26f84516482a6c2de961a3a
Ben Verbraecken <ben.verbraecken@gmail.com>
2023-11-12 19:47:57 +0100

    Start

---
+++ src/main/java/me/quartz/customboxed/CustomBoxed.java
@@ -0,0 +1,16 @@
+package me.quartz.customboxed;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomBoxed extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
